93626abea9bff0dd62fa7278fc938050afdd5c1c
Ijaz Ul Haq <dev.ijazulhaq@gmail.com>
2026-05-24 19:59:46 +0300

    Update frontend - 2026-05-24 19:59:46

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -345,7 +345,9 @@ const Home = () => {
           transition={{ duration: 1.5 }}
           className="absolute inset-0"
           style={{
-            backgroundImage: `url(${background_Image})`,
+            backgroundImage: heroData?.image_url
+              ? `url(${heroData.image_url})`
+              : `url(${background_Image})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
             opacity: 0.15,
